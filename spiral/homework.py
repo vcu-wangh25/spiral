50ca455ca8fe00d8ca818f9efeb139aca2193bc9
Helen Wang <wangh25@vcu.edu>
2021-04-09 23:45:39 -0400

    new

--- spiral/homework.py
+++ spiral/homework.py
@@ -1,4 +1,8 @@
-def spiralize(number):
-    return_value = 1
+def spiralize(number):   
+    if n == 1:
+    return = 1    
 
-    return return_value
+    return (4 * n*n - 6 * n + 6 +
+               spiralize(n-2))
+n = 501
+print(spiralize(n))
